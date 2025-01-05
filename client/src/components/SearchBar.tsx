814dba0323aac937dab0a070445d308c59a8834d
Sayyam Mehra <sayyammehra@gmail.com>
2025-01-05 16:44:51 +0530

    Added debounce in searchbar and optimized product service code

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -2,19 +2,29 @@
 
 import { Input } from './ui/input'
 import { Search } from 'lucide-react'
+import { useState, useEffect } from 'react'
+import { useDebounce } from 'use-debounce'
 
 interface SearchBarProps {
   onSearch: (query: string) => void
 }
 
 export function SearchBar({ onSearch }: SearchBarProps) {
+  const [query, setQuery] = useState('')
+  const [debouncedQuery] = useDebounce(query, 500)
+
+  useEffect(() => {
+    onSearch(debouncedQuery)
+  }, [debouncedQuery, onSearch])
+
   return (
     <div className="relative">
       <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
       <Input
         placeholder="Search by SKU or name..."
         className="pl-8"
-        onChange={(e) => onSearch(e.target.value)}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
     </div>
   )
--- server/src/modules/product/product.service.ts
+++ server/src/modules/product/product.service.ts
@@ -9,129 +9,134 @@ export class ProductService {
     @InjectModel(Product.name) private productModel: Model<Product>,
   ) {}
 
-  async findAll(): Promise<Product[]> {
-    const products = await this.productModel
-      .aggregate([
-        {
-          $group: {
-            _id: '$Handle',
-            Handle: { $first: '$Handle' },
-            Title: { $first: '$Title' },
-            Body: { $first: '$Body' },
-            Vendor: { $first: '$Vendor' },
-            Type: { $first: '$Type' },
-            Tags: { $first: '$Tags' },
-            Option1Name: { $first: '$Option1Name' },
-            Option1Value: { $first: '$Option1Value' },
-            Option2Name: { $first: '$Option2Name' },
-            Option2Value: { $first: '$Option2Value' },
-            Option3Name: { $first: '$Option3Name' },
-            Option3Value: { $first: '$Option3Value' },
-            Variants: {
-              $push: {
-                VariantSKU: '$VariantSKU',
-                VariantGrams: '$VariantGrams',
-                VariantInventoryTracker: '$VariantInventoryTracker',
-                VariantInventoryQty: '$VariantInventoryQty',
-                VariantInventoryPolicy: '$VariantInventoryPolicy',
-                VariantFulfillmentService: '$VariantFulfillmentService',
-                VariantPrice: '$VariantPrice',
-                VariantCompareAtPrice: '$VariantCompareAtPrice',
-                ImageSrc: '$ImageSrc',
-              },
-            },
+  private getCommonGroupStage() {
+    return {
+      $group: {
+        _id: '$Handle',
+        Handle: { $first: '$Handle' },
+        Title: { $first: '$Title' },
+        Body: { $first: '$Body' },
+        Vendor: { $first: '$Vendor' },
+        Type: { $first: '$Type' },
+        Tags: { $first: '$Tags' },
+        Option1Name: { $first: '$Option1Name' },
+        Option1Value: { $first: '$Option1Value' },
+        Option2Name: { $first: '$Option2Name' },
+        Option2Value: { $first: '$Option2Value' },
+        Option3Name: { $first: '$Option3Name' },
+        Option3Value: { $first: '$Option3Value' },
+        Variants: {
+          $push: {
+            VariantSKU: '$VariantSKU',
+            VariantGrams: '$VariantGrams',
+            VariantInventoryTracker: '$VariantInventoryTracker',
+            VariantInventoryQty: '$VariantInventoryQty',
+            VariantInventoryPolicy: '$VariantInventoryPolicy',
+            VariantFulfillmentService: '$VariantFulfillmentService',
+            VariantPrice: '$VariantPrice',
+            VariantCompareAtPrice: '$VariantCompareAtPrice',
+            ImageSrc: '$ImageSrc',
           },
         },
-        {
-          $project: {
-            _id: 0,
-            Handle: 1,
-            Title: 1,
-            Body: 1,
-            Vendor: 1,
-            Type: 1,
-            Tags: 1,
-            Option1Name: 1,
-            Option1Value: 1,
-            Option2Name: 1,
-            Option2Value: 1,
-            Option3Name: 1,
-            Option3Value: 1,
-            Variants: {
-              $map: {
-                input: {
-                  $reduce: {
-                    input: '$Variants',
-                    initialValue: [],
-                    in: {
-                      $concatArrays: [
-                        '$$value',
+      },
+    }
+  }
+
+  private getCommonProjectStage() {
+    return {
+      $project: {
+        _id: 0,
+        Handle: 1,
+        Title: 1,
+        Body: 1,
+        Vendor: 1,
+        Type: 1,
+        Tags: 1,
+        Option1Name: 1,
+        Option1Value: 1,
+        Option2Name: 1,
+        Option2Value: 1,
+        Option3Name: 1,
+        Option3Value: 1,
+        Variants: {
+          $map: {
+            input: {
+              $reduce: {
+                input: '$Variants',
+                initialValue: [],
+                in: {
+                  $concatArrays: [
+                    '$$value',
+                    {
+                      $cond: [
                         {
-                          $cond: [
+                          $and: [
+                            { $ne: ['$$this.VariantSKU', ''] },
                             {
-                              $and: [
-                                { $ne: ['$$this.VariantSKU', ''] },
-                                {
-                                  $not: {
-                                    $in: [
-                                      '$$this.VariantSKU',
-                                      '$$value.VariantSKU',
-                                    ],
-                                  },
-                                },
-                              ],
+                              $not: {
+                                $in: [
+                                  '$$this.VariantSKU',
+                                  '$$value.VariantSKU',
+                                ],
+                              },
                             },
-                            ['$$this'],
-                            [],
                           ],
                         },
+                        ['$$this'],
+                        [],
                       ],
                     },
-                  },
+                  ],
                 },
-                as: 'variant',
-                in: {
-                  VariantSKU: '$$variant.VariantSKU',
-                  VariantGrams: '$$variant.VariantGrams',
-                  VariantInventoryTracker: '$$variant.VariantInventoryTracker',
-                  VariantInventoryQty: '$$variant.VariantInventoryQty',
-                  VariantInventoryPolicy: '$$variant.VariantInventoryPolicy',
-                  VariantFulfillmentService:
-                    '$$variant.VariantFulfillmentService',
-                  VariantPrice: '$$variant.VariantPrice',
-                  VariantCompareAtPrice: '$$variant.VariantCompareAtPrice',
-                  Images: {
-                    $filter: {
-                      input: '$Variants',
-                      as: 'image',
-                      cond: {
-                        $or: [
-                          {
-                            $eq: ['$$image.VariantSKU', '$$variant.VariantSKU'],
-                          },
-                          { $eq: ['$$image.VariantSKU', ''] },
-                        ],
-                      },
-                    },
+              },
+            },
+            as: 'variant',
+            in: {
+              VariantSKU: '$$variant.VariantSKU',
+              VariantGrams: '$$variant.VariantGrams',
+              VariantInventoryTracker: '$$variant.VariantInventoryTracker',
+              VariantInventoryQty: '$$variant.VariantInventoryQty',
+              VariantInventoryPolicy: '$$variant.VariantInventoryPolicy',
+              VariantFulfillmentService: '$$variant.VariantFulfillmentService',
+              VariantPrice: '$$variant.VariantPrice',
+              VariantCompareAtPrice: '$$variant.VariantCompareAtPrice',
+              Images: {
+                $filter: {
+                  input: '$Variants',
+                  as: 'image',
+                  cond: {
+                    $or: [
+                      { $eq: ['$$image.VariantSKU', '$$variant.VariantSKU'] },
+                      { $eq: ['$$image.VariantSKU', ''] },
+                    ],
                   },
                 },
               },
             },
           },
         },
-        {
-          $project: {
-            'Variants.Images.VariantSKU': 0,
-            'Variants.Images.VariantGrams': 0,
-            'Variants.Images.VariantInventoryTracker': 0,
-            'Variants.Images.VariantInventoryQty': 0,
-            'Variants.Images.VariantInventoryPolicy': 0,
-            'Variants.Images.VariantFulfillmentService': 0,
-            'Variants.Images.VariantPrice': 0,
-            'Variants.Images.VariantCompareAtPrice': 0,
-          },
-        },
-      ])
+      },
+    }
+  }
+
+  private getImageCleanupStage() {
+    return {
+      $project: {
+        'Variants.Images.VariantSKU': 0,
+        'Variants.Images.VariantGrams': 0,
+        'Variants.Images.VariantInventoryTracker': 0,
+        'Variants.Images.VariantInventoryQty': 0,
+        'Variants.Images.VariantInventoryPolicy': 0,
+        'Variants.Images.VariantFulfillmentService': 0,
+        'Variants.Images.VariantPrice': 0,
+        'Variants.Images.VariantCompareAtPrice': 0,
+      },
+    }
+  }
+
+  async findAll(): Promise<Product[]> {
+    const products = await this.productModel
+      .aggregate([this.getCommonGroupStage(), this.getCommonProjectStage()])
       .exec()
 
     return products
@@ -149,125 +154,9 @@ export class ProductService {
             ],
           },
         },
-        {
-          $group: {
-            _id: '$Handle',
-            Handle: { $first: '$Handle' },
-            Title: { $first: '$Title' },
-            Body: { $first: '$Body' },
-            Vendor: { $first: '$Vendor' },
-            Type: { $first: '$Type' },
-            Tags: { $first: '$Tags' },
-            Option1Name: { $first: '$Option1Name' },
-            Option1Value: { $first: '$Option1Value' },
-            Option2Name: { $first: '$Option2Name' },
-            Option2Value: { $first: '$Option2Value' },
-            Option3Name: { $first: '$Option3Name' },
-            Option3Value: { $first: '$Option3Value' },
-            Variants: {
-              $push: {
-                VariantSKU: '$VariantSKU',
-                VariantGrams: '$VariantGrams',
-                VariantInventoryTracker: '$VariantInventoryTracker',
-                VariantInventoryQty: '$VariantInventoryQty',
-                VariantInventoryPolicy: '$VariantInventoryPolicy',
-                VariantFulfillmentService: '$VariantFulfillmentService',
-                VariantPrice: '$VariantPrice',
-                VariantCompareAtPrice: '$VariantCompareAtPrice',
-                ImageSrc: '$ImageSrc',
-              },
-            },
-          },
-        },
-        {
-          $project: {
-            _id: 0,
-            Handle: 1,
-            Title: 1,
-            Body: 1,
-            Vendor: 1,
-            Type: 1,
-            Tags: 1,
-            Option1Name: 1,
-            Option1Value: 1,
-            Option2Name: 1,
-            Option2Value: 1,
-            Option3Name: 1,
-            Option3Value: 1,
-            Variants: {
-              $map: {
-                input: {
-                  $reduce: {
-                    input: '$Variants',
-                    initialValue: [],
-                    in: {
-                      $concatArrays: [
-                        '$$value',
-                        {
-                          $cond: [
-                            {
-                              $and: [
-                                { $ne: ['$$this.VariantSKU', ''] },
-                                {
-                                  $not: {
-                                    $in: [
-                                      '$$this.VariantSKU',
-                                      '$$value.VariantSKU',
-                                    ],
-                                  },
-                                },
-                              ],
-                            },
-                            ['$$this'],
-                            [],
-                          ],
-                        },
-                      ],
-                    },
-                  },
-                },
-                as: 'variant',
-                in: {
-                  VariantSKU: '$$variant.VariantSKU',
-                  VariantGrams: '$$variant.VariantGrams',
-                  VariantInventoryTracker: '$$variant.VariantInventoryTracker',
-                  VariantInventoryQty: '$$variant.VariantInventoryQty',
-                  VariantInventoryPolicy: '$$variant.VariantInventoryPolicy',
-                  VariantFulfillmentService:
-                    '$$variant.VariantFulfillmentService',
-                  VariantPrice: '$$variant.VariantPrice',
-                  VariantCompareAtPrice: '$$variant.VariantCompareAtPrice',
-                  Images: {
-                    $filter: {
-                      input: '$Variants',
-                      as: 'image',
-                      cond: {
-                        $or: [
-                          {
-                            $eq: ['$$image.VariantSKU', '$$variant.VariantSKU'],
-                          },
-                          { $eq: ['$$image.VariantSKU', ''] },
-                        ],
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-        {
-          $project: {
-            'Variants.Images.VariantSKU': 0,
-            'Variants.Images.VariantGrams': 0,
-            'Variants.Images.VariantInventoryTracker': 0,
-            'Variants.Images.VariantInventoryQty': 0,
-            'Variants.Images.VariantInventoryPolicy': 0,
-            'Variants.Images.VariantFulfillmentService': 0,
-            'Variants.Images.VariantPrice': 0,
-            'Variants.Images.VariantCompareAtPrice': 0,
-          },
-        },
+        this.getCommonGroupStage(),
+        this.getCommonProjectStage(),
+        this.getImageCleanupStage(),
       ])
       .exec()
 
