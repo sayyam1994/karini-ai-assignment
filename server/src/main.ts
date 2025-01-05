2ae2253272002ed95334098549b99f34068fcabd
Sayyam Mehra <sayyammehra@gmail.com>
2025-01-05 19:00:03 +0530

    added vercel files for client and server and cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
-  app.enableCors()
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
   await app.listen(process.env.PORT || 3001)
 }
 bootstrap()
